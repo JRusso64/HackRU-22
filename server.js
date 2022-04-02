b13a42dee987fdb780499d0af23458d0067953db
MrGorbunov <volarcko@gmail.com>
2022-04-02 17:47:01 -0400

    Added dotenv

--- server.js
+++ server.js
@@ -9,15 +9,14 @@ const cors = require('cors');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const bodyParsers = require('body-parser');
+require('dotenv').config();
 
 const router = express.Router();
 
-// New comment
-
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.use(logger('dev'));
 
+app.use(logger('dev'));
 app.use(cors());
 app.use(bodyParsers.urlencoded({ extended: true }));
 app.use(bodyParsers.json());
