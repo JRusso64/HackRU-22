5e384d8dcca3bfade3273f13d753bcf7aa0e2052
MrGorbunov <volarcko@gmail.com>
2022-04-02 17:25:02 -0400

    Created basic express server

---
+++ server.js
@@ -0,0 +1,25 @@
+/**
+ * Server.js
+ *
+ * Entry point for our server
+ */
+
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const bodyParsers = require('body-parser');
+
+const router = express.Router();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(logger('dev'));
+
+app.use(cors());
+app.use(bodyParsers.urlencoded({ extended: true }));
+app.use(bodyParsers.json());
+
+app.get('/', (req, res) => res.send("Hello!"));
+
+app.listen(PORT, () => console.log(`Now listening on port ${PORT} :)`));
